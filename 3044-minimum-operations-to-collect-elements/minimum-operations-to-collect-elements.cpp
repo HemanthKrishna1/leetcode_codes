0bd620b3d5a9713324da114018e3f1ae150e8b67
Hemanth Krishna <42912017+HemanthKrishna1@users.noreply.github.com>
2023-09-30 22:34:45 -0500

    Time: 4 ms (50.00%) | Memory: 19.2 MB (16.67%) - LeetSync

---
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        set<int> st;
+        for(int i=1;i<=k;i++) st.insert(i);
+        int cnt = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            if(st.size() == 0) break;
+            st.erase(nums[i]);
+            cnt++;
+        }
+        return cnt;
+    }
+};
