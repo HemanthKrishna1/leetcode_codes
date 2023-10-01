d07470d6d8e71969887cb39bfccb2c1878e715cf
Hemanth Krishna <42912017+HemanthKrishna1@users.noreply.github.com>
2023-09-30 22:37:46 -0500

    Time: 6 ms (33.33%) | Memory: 19.3 MB (16.67%) - LeetSync

--- 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.cpp
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.cpp
@@ -5,7 +5,7 @@ public:
         set<int> st;
         for(int i=1;i<=k;i++) st.insert(i);
         int cnt = 0;
-        
+        // traverse ok
         for(int i=n-1;i>=0;i--){
             if(st.size() == 0) break;
             st.erase(nums[i]);
