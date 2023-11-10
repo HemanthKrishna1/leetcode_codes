a2e982b23157a4b64a28b0dcbbfd8eba5bd1c1e9
Hemanth Krishna <42912017+HemanthKrishna1@users.noreply.github.com>
2023-11-09 23:03:21 -0600

    Time: 31 ms (21.19%) | Memory: 74.2 MB (12.27%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -11,52 +11,29 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans1 = new ListNode(0);
-        ListNode* head = ans1;
-        string n1 = "", n2="";
-        while(l1){
-            n1+=to_string(l1->val);
+        ListNode* ans = new ListNode(0);
+        stack<int> s1;
+        stack<int> s2;
+        while(l1!=NULL){
+            s1.push(l1->val);
             l1=l1->next;
         }
-        while(l2){
-            n2+=to_string(l2->val);
+        while(l2!=NULL){
+            s2.push(l2->val);
             l2=l2->next;
         }
-        cout << "n1 " << n1 << endl;
-        cout << "n2 " << n2 << endl;
-        
-        int nn1 = n1.length()-1;
-        int nn2 = n2.length()-1;
-        int carry = 0;
-        string ans = "";
-        while(nn1>=0 && nn2>=0){
-            int sum = carry + n1[nn1]-'0' + n2[nn2]-'0';
-            ans+=to_string(sum%10);
-            carry = sum/10;
-            nn1--;
-            nn2--;
+        int sum = 0;
+        while(!s1.empty() || !s2.empty()){
+            if(!s1.empty()) sum+=s1.top();
+            if(!s2.empty()) sum+=s2.top();
+            ans->val = sum%10;
+            ListNode* head = new ListNode(sum/10);
+            head->next = ans;
+            ans = head;
+            sum/=10;
+            if(!s1.empty())s1.pop();
+            if(!s2.empty())s2.pop();
         }
-        while(nn1 >=0){
-            int sum = carry + n1[nn1]-'0';
-            ans+=to_string(sum%10);
-            carry = sum/10;
-            nn1--;
-        }
-        while(nn2 >=0){
-            int sum = carry + n2[nn2]-'0';
-            ans+=to_string(sum%10);
-            carry = sum/10;
-            nn2--;
-        }
-        if(carry){
-            ans+=to_string(carry);
-        }
-        cout << "ans " << ans << endl;
-        reverse(ans.begin(), ans.end());
-        for(int i=0;i<ans.length();i++){
-            ans1->next = new ListNode(ans[i]-'0');
-            ans1= ans1->next;
-        }
-        return head->next;
+        return ans->val == 0 ? ans->next : ans;
     }
 };
