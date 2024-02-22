00d5ba34af7be461c727ba61eed23abd63444bd1
Hemanth Krishna <42912017+HemanthKrishna1@users.noreply.github.com>
2024-02-21 18:51:03 -0600

    Time: 25 ms (36.36%) | Memory: 10.5 MB (35.42%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.length();
+
+        int i=0,j=0;
+        int start = 0, ans = INT_MAX;
+
+        map<char,int> mpp;
+        for(auto &it: t){
+            mpp[it]++;
+        }
+        int count = mpp.size();
+
+        for(j=0;j<n;j++){
+            if(mpp.find(s[j])!=mpp.end()){
+                mpp[s[j]]--;
+                if(mpp[s[j]] == 0) count--;
+            }
+
+            while(count == 0){
+                if(ans > j - i + 1){
+                    ans = j - i + 1;
+                    start = i;
+                }
+                if(mpp.find(s[i])!= mpp.end()){
+                    if(mpp[s[i]] == 0){
+                        count++;
+                    }
+                    mpp[s[i]]++;
+                }
+                i++;
+            }
+        }
+        return ans == INT_MAX ? "" : s.substr(start, ans);
+    }
+};
